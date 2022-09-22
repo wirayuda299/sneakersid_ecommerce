a90bec54578af56cc9828fab570ce6e1390381ae
wirayuda <wirayuda233@gmail.com>
2022-09-22 16:59:04 +0700

    add bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import 'bootstrap/js/dist/modal'
-import 'bootstrap/js/dist/button'
-import 'bootstrap/js/dist/base-component'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 import toast, {Toaster} from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
